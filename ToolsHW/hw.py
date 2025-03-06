5cea90cb805905c6dde55ab787efef252afde1da
Iane14093051 <tsai960818@gmail.com>
2025-03-06 13:11:11 +0800

    fix: hw.py commit

--- ToolsHW/hw.py
+++ ToolsHW/hw.py
@@ -1,10 +1,8 @@
 import webbrowser, sys, time, random, os  
 
 X1 = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-
+ERROR_COUNT = 0
 def input_math():
-    global ERROR_COUNT
-    ERROR_COUNT = 0
         while True:
             user_input = input("1 times 1 = ? ")
             if user_input == "1": 
@@ -15,7 +13,7 @@ def input_math():
             else:
                 print("Wrong! Try again.")
                 ERROR_COUNT += 1
-
+        
 def open_video():
     webbrowser.open(X1)
     os.system("echo 'Rickroll incoming...'")
@@ -23,15 +21,6 @@ def open_video():
         os.remove("fakefile.txt") 
     except FileNotFoundError:
         pass
-
-
-def func2():
-    try:
-        os.system("echo 'Hello'")
-        os.system("dir")
-        if random.randint(1, 10) > 5:
-            raise ValueError("Fake Error")
-    except ValueError:
-        pass 
+        
 if __name__ == "__main__":
     input_math()
