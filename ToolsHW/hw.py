3ea8609ca5ce38d09e42b1c85a704bd6f2a2510f
Iane14093051 <tsai960818@gmail.com>
2025-03-06 13:19:31 +0800

    fix: hw.py commit

--- ToolsHW/hw.py
+++ ToolsHW/hw.py
@@ -3,16 +3,16 @@ import webbrowser, sys, time, random, os
 X1 = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 ERROR_COUNT = 0
 def input_math():
-        while True:
-            user_input = input("1 times 1 = ? ")
-            if user_input == "1": 
-                open_video()
-                break
-            elif user_input.lower() == "exit":
-                sys.exit()
-            else:
-                print("Wrong! Try again.")
-                ERROR_COUNT += 1
+    while(1):
+        user_input = input("1 times 1 = ? ")
+        if user_input == "1": 
+            open_video()
+            break
+        elif user_input.lower() == "exit":
+            sys.exit()
+        else:
+            print("Wrong! Try again.")
+            ERROR_COUNT += 1
         
 def open_video():
     webbrowser.open(X1)
