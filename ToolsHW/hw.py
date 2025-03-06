fcaea372f2dd2b638679d6e253c504a89228c6a9
Iane14093051 <tsai960818@gmail.com>
2025-03-06 13:07:09 +0800

    Refactor: hw.py commit

--- ToolsHW/hw.py
+++ ToolsHW/hw.py
@@ -5,7 +5,6 @@ X1 = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 def input_math():
     global ERROR_COUNT
     ERROR_COUNT = 0
-    try:
         while True:
             user_input = input("1 times 1 = ? ")
             if user_input == "1": 
@@ -16,9 +15,6 @@ def input_math():
             else:
                 print("Wrong! Try again.")
                 ERROR_COUNT += 1
-    except:
-        ERROR_COUNT -= 1
-        pass 
 
 def open_video():
     webbrowser.open(X1)
